37b3451173929fd26ca57edab5762e99eb2c9925
alfredababio1 <alfred.ababio@cengage.com>
2014-05-23 10:15:03 -0400

    Renamed angular spec location

--- test-angular/spec/controllers/appSpec.js
+++ test-angular/spec/controllers/appSpec.js
@@ -1,19 +1,22 @@
 describe("Unit Testing Examples", function() {
-	var $httpBackend, $rootScope, $scope, createController, appCtrl;
+	var $httpBackend, $rootScope, $scope, appCtrl;
 
-	beforeEach(module('myApp'))
+	beforeEach(module('myApp'));
 	beforeEach(inject(function($injector) {
 		$rootScope = $injector.get('$rootScope');
 		$scope = $rootScope.$new();
 
 		var $controller = $injector.get('$controller');
+		$httpBackend = $injector.get('$http');
 
 		createController = function() {
-			return $controller('appCtrl', { '$scope': $scope });
+			return $controller('appCtrl', { '$scope': $scope, '$http': $httpBackend });
 		};
 	}));
 
 	it('Checking if the controller exists', function() {
-		expect(createController()).toBeDefined();
+		appCtrl = createController();
+		expect(appCtrl).toBeDefined();
 	});
+
 });
