2fecf5dea7d2ee6df1d71002315423c2acbdfe46
Alfred Ababio <aababio75@gmail.com>
2014-12-04 18:29:57 -0500

    Updated modules to latest and updated deprecated code

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 // Dependencies
 var express = require('express');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var morgan = require('morgan');
 
 // Configs
 var db = require('./config/db');
@@ -8,20 +10,22 @@ var db = require('./config/db');
 // Connect to the DB
 mongoose.connect(db.url);
 
+// Initialize the Express App
 var app = express();
-// App Config
-app.configure(function() {
-	// To expose public assets to the world
-	app.use(express.static(__dirname + '/public'));
 
-	// log every request to the console
-	app.use(express.logger('dev'));
+// Configure 
 
-	// For parsing HTTP responses
-	app.use(express.json());
-	app.use(express.urlencoded());
+// To expose public assets to the world
+app.use(express.static(__dirname + '/public'));
 
-});
+// log every request to the console
+app.use(morgan('dev'));
+
+// For parsing HTTP responses
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
 
 // Express Routes
 require('./app/routes/api')(app);
